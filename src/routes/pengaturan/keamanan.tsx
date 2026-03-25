1ba4643e23c70ab20a3c0d7c407495d80db8e408
nico <nicoarya20@gmail.com>
2026-03-25 11:56:22 +0800

    Refactor New Ui Pengaturan Umum

--- src/routes/pengaturan/keamanan.tsx
+++ src/routes/pengaturan/keamanan.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import KeamananSettings from "@/components/pengaturan/keamanan";
 
 export const Route = createFileRoute("/pengaturan/keamanan")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/pengaturan/keamanan"!</div>;
+	return <KeamananSettings />;
 }
