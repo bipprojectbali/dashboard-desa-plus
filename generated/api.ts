6f123aa2db2fbf02bd884129d04d9e9b8c0647c3
nico <nicoarya20@gmail.com>
2026-06-05 10:51:59 +0800

    Nico-5-Juni-26-01

--- generated/api.ts
+++ generated/api.ts
@@ -1015,6 +1015,22 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/api/demografi/export": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: operations["getApiDemografiExport"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/api/demografi/last-sync": {
         parameters: {
             query?: never;
@@ -4307,6 +4323,23 @@ export interface operations {
                         error?: string;
                     };
                 };
+            };
+        };
+    };
+    getApiDemografiExport: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
             };
         };
     };
